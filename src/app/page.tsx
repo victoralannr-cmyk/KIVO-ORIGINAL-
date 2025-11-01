4b88d0c6159c3d08c4099113b634904e08402e83
Victor Alann <victorAlannr@gmail.com>
2025-11-01 23:34:44 +0000

    eu quero que onde esta escrito Preencha com seus dados para começar Noss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,6 @@ export default function Home() {
           <IntegrationsSection />
           <FinalCtaSection />
           <FaqSection />
-        </div>
-        <div className="animated-gradient">
           <ContactSection />
         </div>
       </main>

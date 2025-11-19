641a2ce0baa8922072305a6cf8acd29f0f77a4cd
Victor Alann <victorAlannr@gmail.com>
2025-11-19 01:00:56 +0000

    coloque aqueles pontinhos novamente na primeira aba

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import DotGrid from '../common/dot-grid';
 
 export default function HeroSection() {
     return (
@@ -13,6 +14,16 @@ export default function HeroSection() {
             id="home"
             className="relative w-full pt-40 pb-20 md:pt-64 md:pb-32 flex flex-col justify-center items-center text-center overflow-hidden bg-background"
         >
+            <DotGrid
+              className="absolute inset-0 z-0"
+              dotSize={2}
+              gap={24}
+              baseColor="#2A334A"
+              activeColor="#F0F0F0"
+              proximity={120}
+              resistance={500}
+              returnDuration={0.8}
+            />
 
             <div className={cn("container relative z-10 px-4 md:px-6 flex flex-col justify-center items-center h-full text-center")}>
                 <div className="flex-grow flex flex-col justify-center items-center">
@@ -37,5 +48,3 @@ export default function HeroSection() {
         </section>
     );
 }
-
-    

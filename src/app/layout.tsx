2f62053c1b54703e6d8fe96b03ff2308a0c933c6
Victor Alann <victorAlannr@gmail.com>
2025-11-20 04:41:33 +0000

    mude apenas a paleta de cores do plano de fundo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       </head>
       <body className={cn("font-body antialiased text-foreground")}>
         <FloatingLines 
-          linesGradient={['#0b0b64', '#151515', '#0b0b64']}
+          linesGradient={['#2ED5A9', '#151515', '#2ED5A9']}
           mixBlendMode="screen"
         />
         <div className="relative z-10 app-container">

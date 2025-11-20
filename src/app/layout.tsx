eef660f7b8c12829ef50b8f86ecfc1722d428329
Victor Alann <victorAlannr@gmail.com>
2025-11-20 03:34:24 +0000

    exclua

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("font-body antialiased")}>
-        <div className="linhas-animadas" />
-        <div className="vignette-overlay" />
         <div className="relative z-10">
           {children}
         </div>

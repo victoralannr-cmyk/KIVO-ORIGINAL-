7dd6135cc2abf4a60e6d3f61eba4f2a635360222
Victor Alann <victorAlannr@gmail.com>
2025-11-25 20:46:52 +0000

    ajuste o do setor criatoivo não esta cabendo tudo aumente um pouco mais

--- src/components/sections/creative-workflow-card.tsx
+++ src/components/sections/creative-workflow-card.tsx
@@ -57,38 +57,38 @@ export default function CreativeWorkflowCard() {
         }
       `}</style>
       <div className="workflow-diagram-container">
-        <svg className="animated-svg" viewBox="0 0 400 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="400" height="190" rx="20" fill="#19233A"/>
+        <svg className="animated-svg" viewBox="0 0 400 220" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="400" height="220" rx="20" fill="#19233A"/>
 
           {/* Lines */}
-          <path className="connector-line line-1" d="M200 64 V 94" stroke="#F8F9FB" strokeWidth="1.5" strokeOpacity="0.5"/>
-          <path className="connector-line line-2" d="M200 126 V 156" stroke="#F8F9FB" strokeWidth="1.5" strokeOpacity="0.5"/>
+          <path className="connector-line line-1" d="M200 70 V 100" stroke="#F8F9FB" strokeWidth="1.5" strokeOpacity="0.5"/>
+          <path className="connector-line line-2" d="M200 140 V 170" stroke="#F8F9FB" strokeWidth="1.5" strokeOpacity="0.5"/>
 
           {/* Box 1: Planejamento */}
-          <g className="box box-1" transform="translate(100, 0)">
-            <rect x="50" y="25" width="150" height="40" rx="10" stroke="#F8F9FB" strokeWidth="2" fill="none"/>
-            <foreignObject x="65" y="35" width="20" height="20" className="icon icon-1">
+          <g className="box box-1">
+            <rect x="125" y="30" width="150" height="40" rx="10" stroke="#F8F9FB" strokeWidth="2" fill="none"/>
+            <foreignObject x="135" y="40" width="20" height="20" className="icon icon-1">
                 <Settings size={20} color="#888" />
             </foreignObject>
-            <text x="95" y="50" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-1">Planejamento</text>
+            <text x="165" y="55" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-1">Planejamento</text>
           </g>
           
           {/* Box 2: Roterização */}
-          <g className="box box-2" transform="translate(100, 0)">
-            <rect x="50" y="95" width="150" height="40" rx="10" stroke="#F8F9FB" strokeWidth="2" fill="none"/>
-             <foreignObject x="65" y="105" width="20" height="20" className="icon icon-2">
+          <g className="box box-2">
+            <rect x="125" y="100" width="150" height="40" rx="10" stroke="#F8F9FB" strokeWidth="2" fill="none"/>
+             <foreignObject x="135" y="110" width="20" height="20" className="icon icon-2">
                 <Mail size={20} color="#888" />
             </foreignObject>
-            <text x="95" y="120" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-2">Roteirização</text>
+            <text x="165" y="125" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-2">Roteirização</text>
           </g>
 
           {/* Box 3: Produção */}
-          <g className="box box-3" transform="translate(100, 0)">
-            <rect x="50" y="155" width="150" height="40" rx="10" stroke="hsl(var(--primary))" strokeWidth="2.5" fill="none"/>
-             <foreignObject x="65" y="165" width="20" height="20" className="icon icon-3">
+          <g className="box box-3">
+            <rect x="125" y="170" width="150" height="40" rx="10" stroke="hsl(var(--primary))" strokeWidth="2.5" fill="none"/>
+             <foreignObject x="135" y="180" width="20" height="20" className="icon icon-3">
                 <Video size={20} color="hsl(var(--primary))" />
             </foreignObject>
-            <text x="95" y="180" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-3">Produção</text>
+            <text x="165" y="195" fontFamily="Poppins, sans-serif" fontSize="14" fill="#F8F9FB" className="text text-3">Produção</text>
           </g>
         </svg>
       </div>

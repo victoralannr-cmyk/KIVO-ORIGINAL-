115c58792e7fcb5a56e91f4a306aa382720744df
Victor Alann <victorAlannr@gmail.com>
2025-11-27 23:30:05 +0000

    coloque apenas o claquete

--- src/components/sections/how-it-works.tsx
+++ src/components/sections/how-it-works.tsx
@@ -134,26 +134,9 @@ export default function HowItWorksSection() {
         );
       case 'Clapperboard':
         return (
-           <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="text-primary"
-            >
-              <g className="animate-clap">
-                <path d="M4 11.9 10.1 3" />
-                <path d="m5.2 6.3 5-2.7" />
-                 <path d="M14 11.9 20.1 3" />
-              </g>
-              <path d="M4 12h16" />
-              <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
-            </svg>
+           <Clapperboard
+              className="text-primary h-8 w-8"
+            />
         );
       default:
         return null;

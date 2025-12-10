1335ab8efc14c3db1524d36732779c1a1b30cef7
Victor Alann <victorAlannr@gmail.com>
2025-12-10 05:22:53 +0000

    coloque uma seta abaixo de onde esta escrito Tráfego colocam o seu negóc

--- src/components/sections/video-cta.tsx
+++ src/components/sections/video-cta.tsx
@@ -53,6 +53,13 @@ export default function VideoCtaSection() {
             <span className="text-wavy-gradient">olhares certos.</span> O seu posicionamento é a sua{' '}
             <span className="text-wavy-gradient">vitrine digital!</span>
           </h2>
+
+          <div className="mt-8 flex justify-center text-muted-foreground animate-bounce">
+              <svg width="59.22" height="78.96" viewBox="0 0 59.22 78.96" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M29.61 1V77.96" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M58.22 50.34L29.61 77.95L1 50.34" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+          </div>
         </motion.div>
       </div>
     </section>
